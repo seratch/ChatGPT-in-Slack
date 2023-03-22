190f79603b21969c50236f876c7bee19f44d8005
Chris Renton <crenton@me.com>
2023-03-22 20:15:12 +1100

    Add Slack user IDs to messages sent to OpenAI, strip any user ID that appears at the start of the response and strip bot Slack user ID from initial message

--- app/env.py
+++ app/env.py
@@ -5,6 +5,7 @@ You are a bot in a slack chat room. You might receive messages from multiple peo
 Format bold text *like this*, italic text _like this_ and strikethrough text ~like this~.
 Slack user IDs match the regex `<@U.*?>`.
 Your Slack user ID is <@{bot_user_id}>.
+Each message has the author's Slack user ID prepended, like the regex `^<@U.*?>: ` followed by the message text.
 """
 SYSTEM_TEXT = os.environ.get("OPENAI_SYSTEM_TEXT", DEFAULT_SYSTEM_TEXT)
 
