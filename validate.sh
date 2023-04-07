96005af6c0b9e178c8da09e333e2a93ecf7aa0fb
Chris Renton <crenton@me.com>
2023-04-07 15:55:07 +1000

    Validate check app and tests directories

--- validate.sh
+++ validate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pip install -r requirements.txt
-pip install black && black ./*.py
+pip install black && black ./*.py ./app/*.py ./tests/*.py
 pip install pytest && pytest .
-pip install "flake8==6.0.0" && flake8 ./*.py
-pip install "pytype==2023.03.02" boto3 && pytype ./*.py
+pip install "flake8==6.0.0" && flake8 ./*.py ./app/*.py ./tests/*.py
+pip install "pytype==2023.03.02" boto3 && pytype ./*.py ./app/*.py ./tests/*.py
