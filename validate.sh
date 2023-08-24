5e558f2d719bde92ac4b122e459ec1812ca68114
Kazuhiro Sera <seratch@gmail.com>
2023-08-24 18:53:39 +0900

    Upgrade dependencies

--- validate.sh
+++ validate.sh
@@ -2,5 +2,5 @@
 pip install -r requirements.txt
 pip install black && black ./*.py ./app/*.py ./tests/*.py
 pip install pytest && pytest .
-pip install "flake8==6.0.0" && flake8 ./*.py ./app/*.py ./tests/*.py
-pip install "pytype==2023.4.11" boto3 && pytype ./*.py ./app/*.py ./tests/*.py
+pip install "flake8==6.1.0" && flake8 ./*.py ./app/*.py ./tests/*.py
+pip install "pytype==2023.8.22" boto3 && pytype ./*.py ./app/*.py ./tests/*.py
