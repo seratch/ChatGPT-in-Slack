dfdcc1d029f2d8bd0825134d8cf4231a665bf80b
Marat Dasaev <marat.dasaev@gmail.com>
2024-09-21 10:21:57 +0300

    fixin tf run

--- terraform/output.tf
+++ terraform/output.tf
@@ -5,17 +5,6 @@ output "aws_region" {
 
 
 
-output "publisher_access_key" {
-  value       = aws_iam_access_key.publisher.id
-  description = "AWS_ACCESS_KEY to publish to ECR"
-}
-
-output "publisher_secret_key" {
-  value       = aws_iam_access_key.publisher.secret
-  description = "AWS_SECRET_ACCESS_KEY to upload to the ECR"
-  sensitive   = true
-}
-
 output "ecr_url" {
   value       = aws_ecr_repository.repository.repository_url
   description = "The ECR repository URL"
